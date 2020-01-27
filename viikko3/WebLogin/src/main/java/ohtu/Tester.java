25735d1f655da90179e17abd7650cad2998499fd
Miika Somero <miika.somero@gmail.com>
2020-01-27 11:05:21 +0200

    viikon 3 tehtävät 8 saakka

---
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -0,0 +1,88 @@
+package ohtu;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:4567");
+        
+        sleep(2);
+        
+        
+//        scenario: login with valid credentials
+//        
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        sleep(2);
+//
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+//        
+//        sleep(2);
+//        element.submit();
+//
+//        sleep(3);
+        
+//        skenaario: epäonnistunut kirjautuminen: oikea käyttäjätunnus, väärä salasana
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//        
+//        sleep(2);
+//        
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("wrong");
+//        element = driver.findElement(By.name("login"));
+//        sleep(2);
+//        element.submit();
+//        sleep(3);
+        
+        
+        // skenaario: uuden käyttäjätunnuksen luominen
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        
+        sleep(2);
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("miika" + new Random().nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("salainen");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("salainen");
+        element = driver.findElement(By.name("signup"));
+        sleep(2);
+        element.submit();
+        sleep(3);
+        
+        
+        // skenaario: uuden käyttäjätunnuksen luomisen jälkeen tapahtuva ulkoskirjautuminen sovelluksesta
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        sleep(3);
+        
+        driver.quit();
+    }
+    
+    private static void sleep(int n){
+        try{
+            Thread.sleep(n*1000);
+        } catch(Exception e){}
+    }
+}
